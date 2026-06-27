f37d38fdfa40618b835c5d9e58c28b68bfab8527
Akshay-hub-007 <akshaykalangi9@gmail.com>
2026-06-27 22:54:28 +0530

    github actions push

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
-export const metadata= {
-    title: "Home Pge",
+export const metadata = {
+  title: "Home Pge",
   description: "Welcome to my website",
 }
 
@@ -8,15 +8,18 @@ export default async function Home() {
   const res = await fetch(
     "http://localhost:3000/api/user",
     {
-      method:"POST",
-      body:JSON.stringify({
-        name :"ajshay",
-        age:34
+      method: "POST",
+      body: JSON.stringify({
+        name: "ajshay",
+        age: 34
       })
     }
   );
 
   const data = await res.json();
 
-  return <div>{data.name}</div>;
+  return <>
+    <div>{data.name}</div>
+    <p>Github actions added</p>
+  </>;
 }
