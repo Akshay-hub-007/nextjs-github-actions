12a172efafe2ffd1cc0c27927b8250e04f49eedc
Akshay-hub-007 <akshaykalangi9@gmail.com>
2026-06-28 00:43:46 +0530

    changes made

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default  function Home() {
 
   return <>
     <div>
-      <h3>Counter</h3>
+      <h3>Counter 12</h3>
       <h1>{count}</h1>
       <button className="w-12 h-23 bg-emerald-500" onClick={()=>setCount(cnt => cnt+1)}>+</button>
        <button className="w-12 h-23 bg-amber-800" onClick={()=>setCount(cnt => cnt-1)}>-</button>
